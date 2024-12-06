91893d4cb628addfda76855a3c098c88ec708f10
Luca Gazzardi <gazzardi.luca@gmail.com>
2024-12-06 12:08:16 +0100

    initial setup - services creation - docker-compose and dockerfiles

---
+++ kubernetes-groceries/Catalog_API/Controllers/CatalogController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "One day I will return the catalog... maybe";
+        }
+    }
+}
---
+++ kubernetes-groceries/List_API/Controllers/ListController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace List_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListController : ControllerBase
+    {
+        private readonly ILogger<ListController> _logger;
+
+        public ListController(ILogger<ListController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "One day I will return the groceries lists... maybe";
+        }
+    }
+}
